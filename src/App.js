12ab61bd910be1a603f5e7a8e565c071f4a6285e
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-09-12 16:02:42 +0530

    entrollment form added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
-import FormikContainer from "./components/FormikContainer";
-import LoginForm from "./components/LoginForm";
-import RegistrationForm from "./components/RegistrationForm";
+import EnrollmentForm from "./components/EnrollmentForm";
+// import FormikContainer from "./components/FormikContainer";
+// import LoginForm from "./components/LoginForm";
+// import RegistrationForm from "./components/RegistrationForm";
 
 function App() {
   return (
     <div className="App">
       {/* <FormikContainer /> */}
       {/* <LoginForm /> */}
-      <RegistrationForm />
+      {/* <RegistrationForm /> */}
+      <EnrollmentForm />
     </div>
   );
 }
